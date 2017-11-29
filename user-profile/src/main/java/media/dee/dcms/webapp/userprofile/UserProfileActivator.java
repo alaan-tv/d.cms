2a3a748c9cdf43ecbb482ba34334a884caee3fee
Anas Aswad <anas@alaan.tv>
2017-11-30 03:33:16 +0400

    Using Declartive Service Annotations

--- user-profile/src/main/java/media/dee/dcms/webapp/userprofile/UserProfileActivator.java
+++ user-profile/src/main/java/media/dee/dcms/webapp/userprofile/UserProfileActivator.java
@@ -1,48 +1,18 @@
 package media.dee.dcms.webapp.userprofile;
 
-import media.dee.dcms.webapp.cms.MenuItem;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
-import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.http.HttpService;
 import org.osgi.util.tracker.ServiceTracker;
 
 public class UserProfileActivator implements BundleActivator {
-    private ServiceRegistration<MenuItem> registration;
-    private ServiceTracker httpTracker;
 
     public void start(BundleContext context) throws Exception {
-        registration = context.registerService(MenuItem.class, new UserProfileMenuItem(), null);
 
-        httpTracker = new ServiceTracker<HttpService,HttpService>(context, HttpService.class.getName(), null) {
-            public void removedService(ServiceReference<HttpService> reference, HttpService service) {
-                // HTTP service is no longer available, unregister our resources...
-                try {
-                    service.unregister("/cms/js/layout/userprofile");
-                } catch (IllegalArgumentException exception) {
-                    // Ignore; servlet registration probably failed earlier on...
-                }
-            }
 
-            public HttpService addingService(ServiceReference<HttpService> reference) {
-                // HTTP service is available, register our resources...
-                HttpService httpService = this.context.getService(reference);
-                try {
-                    httpService.registerResources("/cms/js/layout/userprofile", "/webapp/js/layout/userprofile", null);
-                } catch (Exception exception) {
-                    exception.printStackTrace();
-                }
-                return httpService;
-            }
-        };
-        // start tracking all HTTP ServiceReferences...
-        httpTracker.open();
     }
 
     public void stop(BundleContext context) throws Exception {
-        registration.unregister();
-        // stop tracking all HTTP ServiceReferences...
-        httpTracker.close();
     }
 }
--- user-profile/src/main/java/media/dee/dcms/webapp/userprofile/UserProfileMenuItem.java
+++ user-profile/src/main/java/media/dee/dcms/webapp/userprofile/UserProfileMenuItem.java
@@ -1,14 +1,59 @@
 package media.dee.dcms.webapp.userprofile;
 
 import media.dee.dcms.webapp.cms.MenuItem;
+import org.osgi.service.component.annotations.*;
+import org.osgi.service.http.HttpService;
+import org.osgi.service.log.LogService;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 
+@Component
 public class UserProfileMenuItem implements MenuItem{
+    private final AtomicReference<LogService> logRef = new AtomicReference<>();
+
+    @Reference(
+            cardinality=ReferenceCardinality.MULTIPLE,
+            policy=ReferencePolicy.DYNAMIC
+    )
+    public void setLog( LogService log ) {
+        logRef.set(log);
+    }
+
+    public void unsetLog( LogService log ) {
+        logRef.compareAndSet(log, null);
+    }
+
     @Override
     public List<String> getJavascriptModules() {
         return Arrays.asList("js/layout/userprofile/userprofile");
+    }
+
+    @Activate
+    void activate() {
+        LogService log = logRef.get();
+        log.log(LogService.LOG_INFO, "UserProfileMenuItem Activated");
+    }
+
+    @Reference(
+            cardinality= ReferenceCardinality.MULTIPLE,
+            policy= ReferencePolicy.DYNAMIC
+    )
+    void setHttpService( HttpService httpService) {
+        try {
+            httpService.registerResources("/cms/js/layout/userprofile", "/webapp/js/layout/userprofile", null);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    void unsetHttpService(HttpService httpService){
+        try {
+            httpService.unregister("/cms/js/layout/userprofile");
+        } catch (IllegalArgumentException exception) {
+            // Ignore; servlet registration probably failed earlier on...
+        }
     }
 }
