4a8c472cc6d8f226e5433f1c5e519e9808de6b93
Anas Aswad <anas@alaan.tv>
2017-12-20 21:16:23 +0400

    POS: nav and routers

--- cms-web-app/src/main/front-end/javascript/containers/Full/Full.js
+++ cms-web-app/src/main/front-end/javascript/containers/Full/Full.js
@@ -1,36 +1,84 @@
 import React, {Component} from 'react';
-import {Link, Switch, Route, Redirect} from 'react-router-dom';
+import {Redirect, Route, Switch} from 'react-router-dom';
 import {Container} from 'reactstrap';
 import Header from '../../components/Header';
 import Sidebar from '../../components/Sidebar';
-import Breadcrumb from '../../components/Breadcrumb/';
-import Aside from '../../components/Aside/';
-import Footer from '../../components/Footer/';
+import Breadcrumb from '../../components/Breadcrumb';
+import Aside from '../../components/Aside';
+import Footer from '../../components/Footer';
 
-import Dashboard from '../../views/Dashboard/';
+import Dashboard from '../../views/Dashboard';
+import {ServiceTracker} from "../../service/ServiceTracker";
 
 class Full extends Component {
-  render() {
-    return (
-      <div className="app">
-        <Header />
-        <div className="app-body">
-          <Sidebar {...this.props}/>
-          <main className="main">
-            <Breadcrumb />
-            <Container fluid>
-              <Switch>
-                <Route path="/dashboard" name="Dashboard" component={Dashboard}/>
-                <Redirect from="/" to="/dashboard"/>
-              </Switch>
-            </Container>
-          </main>
-          <Aside />
-        </div>
-        <Footer />
-      </div>
-    );
-  }
+    constructor(props){
+        super(props);
+        this.state = {
+            routers: []
+        }
+    }
+
+    componentWillMount() {
+        this.serviceTracker = new ServiceTracker(
+            bundleContext,
+            'd.cms.ui.router',
+            null,
+            (context, serviceReference) => {
+                this.setState( (prevState) => {
+                    let routers = serviceReference.getService(context);
+                    let order = serviceReference.props.order || 'last';
+                    switch (order){
+                        case 'first':
+                            return {
+                                routers: [...routers, ...prevState.routers]
+                            };
+                        case 'last':
+                        default:
+                            return {
+                                routers: [...prevState.routers, ...routers]
+                            };
+                    }
+                })
+            },
+            (context, serviceReference, routerInfoList) => {
+                this.setState( (prevState) => {
+                    let routers = prevState.routers.slice();
+                    routerInfoList.forEach( (router) => {
+                        let indx = routers.indexOf(router);
+                        if (indx >= 0)
+                            routers.splice(indx, 1);
+                    });
+                    return {
+                        routers: routers
+                    };
+                });
+            }
+        )
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <Header/>
+                <div className="app-body">
+                    <Sidebar {...this.props}/>
+                    <main className="main">
+                        <Breadcrumb/>
+                        <Container fluid>
+                            <Switch>
+                                {this.state.routers.map( (router, indx) =>
+                                    <Route key={indx} path={router.path} name={router.name} component={router.component}/>
+                                )}
+                                <Redirect from="/" to="/dashboard"/>
+                            </Switch>
+                        </Container>
+                    </main>
+                    <Aside/>
+                </div>
+                <Footer/>
+            </div>
+        );
+    }
 }
 
 export default Full;
