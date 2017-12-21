f9fed493f1b1ba74e4a82520537a9a5e4bcf14ff
Anas Aswad <anas@alaan.tv>
2017-12-22 01:12:42 +0400

    CMS: simplify http registration

--- cms-web-app/src/main/java/media/dee/dcms/webapp/cms/internal/CMSApplicationActivator.java
+++ cms-web-app/src/main/java/media/dee/dcms/webapp/cms/internal/CMSApplicationActivator.java
@@ -2,44 +2,14 @@ package media.dee.dcms.webapp.cms.internal;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
-import org.osgi.service.http.HttpService;
-import org.osgi.util.tracker.ServiceTracker;
 
 public class CMSApplicationActivator implements BundleActivator {
-    private ServiceTracker httpTracker;
 
     public void start(BundleContext context) throws Exception {
-        httpTracker = new ServiceTracker<HttpService,HttpService>(context, HttpService.class.getName(), null) {
-            public void removedService(ServiceReference<HttpService> reference, HttpService service) {
-                // HTTP service is no longer available, unregister our resources...
-                try {
-                    service.unregister("/cms");
-                    service.unregister("/cms/fe");
-                } catch (IllegalArgumentException exception) {
-                    // Ignore; servlet registration probably failed earlier on...
-                }
-            }
 
-            public HttpService addingService(ServiceReference<HttpService> reference) {
-                // HTTP service is available, register our resources...
-                HttpService httpService = this.context.getService(reference);
-                try {
-                    httpService.registerServlet("/cms", new IndexServlet(), null, null);
-                    httpService.registerResources("/cms/fe", "/webapp", null);
-                } catch (Exception exception) {
-                    exception.printStackTrace();
-                }
-                return httpService;
-            }
-        };
-        // start tracking all HTTP ServiceReferences...
-        httpTracker.open();
 
     }
 
     public void stop(BundleContext context) throws Exception {
-        // stop tracking all HTTP ServiceReferences...
-        httpTracker.close();
     }
 }
--- cms-web-app/src/main/java/media/dee/dcms/webapp/cms/internal/IndexServlet.java
+++ cms-web-app/src/main/java/media/dee/dcms/webapp/cms/internal/IndexServlet.java
@@ -1,5 +1,11 @@
 package media.dee.dcms.webapp.cms.internal;
 
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.http.HttpService;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,7 +15,28 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+@Component(immediate = true)
 public class IndexServlet extends HttpServlet {
+
+    @Reference(cardinality = ReferenceCardinality.AT_LEAST_ONE, unbind = "unbindHttpService", policy = ReferencePolicy.DYNAMIC)
+    public void bindHttpService( HttpService httpService ) {
+        try {
+            httpService.registerServlet("/cms", this, null, null);
+            httpService.registerResources("/cms/fe", "/webapp", null);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    public void unbindHttpService(HttpService httpService){
+        try {
+            httpService.unregister("/cms");
+            httpService.unregister("/cms/fe");
+        } catch (IllegalArgumentException exception) {
+            // Ignore; servlet registration probably failed earlier on...
+        }
+    }
+
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         InputStream in = getServletContext().getResourceAsStream("/webapp/index.html");
         OutputStream os = resp.getOutputStream();
